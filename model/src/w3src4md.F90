20fc1f6befcb667eade31d28a6860ebd3103e081
Miguel Solano <miguel.solano.cordoba@gmail.com>
2024-03-25 23:39:17 +0000

    Fix gustiness bug, as suggst by Pieter

--- model/src/w3src4md.F90
+++ model/src/w3src4md.F90
@@ -684,7 +684,7 @@ CONTAINS
     ELSE
       Usigma = MAX(0.,-0.025*AS)
     END IF
-    USTARsigma=(1.0+U/(10.+U))*Usigma
+    USTARsigma=(1.0+0.5*U/(10.+U))*Usigma
 #endif
 #ifdef W3_T
     WRITE (NDST,9003) AS, Usigma, USTARsigma, U
